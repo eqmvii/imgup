a3488250a3b5cc3852c7823e14ca60aacef14cd1
eqmvii <coelomate@gmail.com>
2025-01-17 10:37:04 -0500

    working on localhost:8000 yay!

--- config/dev.exs
+++ config/dev.exs
@@ -9,7 +9,8 @@ import Config
 config :imgup_app, ImgupAppWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  # NOTE: This is needed to get the docker connection working for local development!
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
