1b1f39d1a75377ebbff4d520ac83c85abce24b48
eqmvii <coelomate@gmail.com>
2025-01-23 19:46:54 -0500

    IT WORKEDgit add -Agit add -Agit add -Agit add -Agit add -Agit add -A

--- lib/imgup_app_web/controllers/upload_controller.ex
+++ lib/imgup_app_web/controllers/upload_controller.ex
@@ -3,10 +3,13 @@ defmodule ImgupAppWeb.UploadController do
 
   def upload(conn, %{"file" => upload} = params) do
     # Randomly test ex aws functionality via listing s3 objects in test bucket
-    raise inspect ExAws.S3.list_objects(System.get_env("IMGUP_BUCKET_NAME")) |> ExAws.request()
+    # raise inspect ExAws.S3.list_objects(System.get_env("IMGUP_BUCKET_NAME")) |> ExAws.request()
 
     # Extract file information for debugging during testing
     %Plug.Upload{filename: filename, path: path, content_type: content_type} = upload
+
+    timestamp = :os.system_time(:second)
+    raise inspect upload_file(path, "local/hacking/imgup_testing_#{filename}_#{timestamp}")
 
     # TODO cleanup
     # raise inspect params, pretty: true
@@ -16,9 +19,26 @@ defmodule ImgupAppWeb.UploadController do
     IO.puts("Path: #{path}\n")
     IO.puts("Content type: #{content_type}\n")
 
-    # Respond to the client
     conn
     |> put_flash(:info, "File #{filename} uploaded successfully!")
     |> redirect(to: "/")
   end
+
+  ###################
+  # Private Methods #
+  ###################
+
+  defp upload_file(file_path, s3_key) do
+    file_path
+    |> File.read()
+    |> case do
+      {:ok, file_content} ->
+        ExAws.S3.put_object(System.get_env("IMGUP_BUCKET_NAME"), s3_key, file_content)
+        |> ExAws.request()
+
+      {:error, reason} ->
+        {:error, "Failed to read file: #{reason}"}
+    end
+  end
+
 end
